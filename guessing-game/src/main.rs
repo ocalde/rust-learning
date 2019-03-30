f9e33e74b42e8a428e77bc5542df2b7e01065732
Oscar Calderon <oscar@calderon.cn>
2019-03-29 18:29:20 -0600

    Tweaked guessing game a little bit

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,48 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("::::Guess the number::::");
+    let machine_guess = rand::thread_rng().gen_range(1, 100);
+    let mut number_of_tries: u8 = 0;
+    let mut number_of_invalid_inputs: u8 = 0;
+    let max_invalid_inputs: u8 = 5;
+
+    loop {
+        println!("Input your guess: ");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess = match guess.trim().parse::<i8>() {
+            Ok(num) => num,
+            Err(_) => {
+                
+                number_of_invalid_inputs += 1;
+                match number_of_invalid_inputs.cmp(&max_invalid_inputs) {
+                    Ordering::Equal => {
+                        println!("You don't want to play, get away!!");
+                        break;
+                    },
+                    Ordering::Less | Ordering::Greater => println!("Don't be a piece of cake, enter a valid number please!"),
+                }
+                continue;
+            },
+        };
+            //    .expect("Please type a number little shitty");
+
+        match guess.cmp(&machine_guess) {
+            Ordering::Equal => {
+                println!("You guessed!! {}", guess);
+                println!("It took you {} attempts to guess it", number_of_tries);
+                break;
+            },
+            Ordering::Less => println!("Too low!!"),
+            Ordering::Greater => println!("Too high!! "),
+            
+        }    
+        number_of_tries += 1;
+    }
+}
